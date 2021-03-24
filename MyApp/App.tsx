7c553f881921242ff987e7b724abe15ea6a403e0
Joshua Blankenship <joshua.blankenship@dxc.com>
2021-03-23 19:58:11 -0500

    google login working

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -1,12 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
 
  import React from 'react';
  import {
@@ -16,7 +7,6 @@
 import Login from './containers/Login';
 
  const App = () => {
-
 
    return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
