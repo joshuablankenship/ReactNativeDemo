4cb35b67628b49a6adfb46e37cd6ae76529f3076
Joshua Blankenship <joshua.blankenship@dxc.com>
2021-03-24 17:36:24 -0500

    layout for description page

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
 import 'react-native-gesture-handler';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+//@ts-ignore
 import {createStackNavigator} from '@react-navigation/stack';
 import Login from './containers/Login';
+import Main from './containers/Main';
+import Description from './containers/Description';
 
 const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+      <Stack.Screen
+          name="Description"
+          component={Description}
+          options={{title: 'Cat Facts', headerTintColor: 'black'}}
+        />
         <Stack.Screen
           name="Login"
           component={Login}
           options={{title: 'Cat Facts', headerTintColor: 'black'}}
+        />
+        <Stack.Screen
+          name="Main"
+          component={Main}
+          options={{title: 'Select a Breed', headerTintColor: 'black'}}
         />
       </Stack.Navigator>
     </NavigationContainer>
