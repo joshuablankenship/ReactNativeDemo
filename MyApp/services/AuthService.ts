99d3e3e416d890e1b05b1c19e2e03748b7a27730
Joshua Blankenship <joshua.blankenship@dxc.com>
2021-03-23 19:54:59 -0500

    added config for auth

---
+++ MyApp/services/AuthService.ts
@@ -0,0 +1,10 @@
+// @ts-ignore
+import { ISSUER, CLIENT_ID, REDIRECT_URL } from 'react-native-dotenv';
+const config = {
+    issuer: ISSUER,
+    clientId: CLIENT_ID,
+    redirectUrl: REDIRECT_URL,
+    scopes: ['openid', 'profile']
+  };
+
+export default config;
